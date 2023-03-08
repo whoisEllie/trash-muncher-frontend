2cb6509eac5bdc93aad923e5163ebbd0cbea0b67
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2023-03-08 15:12:18 +0000

    dynamic grid
    removed the body tag i guess

--- src/routes/gamekeeper/image-approve/+page.svelte
+++ src/routes/gamekeeper/image-approve/+page.svelte
@@ -5,15 +5,33 @@
 	
 	function addDiv(event) {
 		count += 1
+		
 		var clone = item[0].cloneNode(true)
-		var cloneButton = remove.cloneNode(true)
+		var removeButton = remove.cloneNode(true)
 		
 		clone.id = "item" + count
 		grid.appendChild(clone)
-		clone.appendChild(cloneButton)
+				
+		var image = document.createElement("img")
+		image.style.height = "100%"
 		
-		cloneButton.onclick = function() {
-			this.parentNode.remove();
+		// example of changing image source below
+		
+		/*if (count == 1) {
+			image.src = '/images/the-blowup-t06aN6vewaQ-unsplash.jpg'
+		}
+		
+		if (count == 2) {
+			image.src = '/images/tempmap.png'
+		}*/
+		
+		clone.appendChild(image)
+		
+		removeButton.style.visibility = "visible";
+		clone.appendChild(removeButton)
+		
+		removeButton.onclick = function() {
+			this.parentNode.remove()
 			count -= 1
 		}
 		
@@ -21,26 +39,25 @@
 	}
 </script>
 
-<body>
-	<div class = "grid" bind:this={grid}>
-		<div class = "add">
-			<button type="button" name="add" on:click={addDiv}>+</button>
-		</div>
+<div class = "grid" bind:this={grid}>
+	<div class = "add">
+		<button type="button" name="add" on:click={addDiv}>+</button>
 	</div>
-	<div class = "item" bind:this={item[count]}>
-		<!-- add whatever here -->
-	</div>
-	<button type="button" name="remove" bind:this={remove}>X</button>
-</body>
+</div>
+
+<div class = "item" bind:this={item[count]}>
+	<!-- template div -->
+</div>
+
+<button type="button" class="remove" bind:this={remove} style="visibility: hidden;">X</button>
 
 <style>
 	.grid {
 		position: absolute;
 		width: 70%;
 		height: 80%;
-		display: grid;
-		grid-template-columns: repeat(4, 1fr); /* change 4 to change number of cols/rows (16x16 currently) */
-		grid-template-rows: repeat(4, 1fr);
+		display: flex;
+		flex-flow: wrap;
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
@@ -48,7 +65,9 @@
 	}
 	
 	.item{
-		background-color: #DEDEDE;
+		max-height: 100%;
+		height: 50%;
+
 	}
 	
 	.item > img {
