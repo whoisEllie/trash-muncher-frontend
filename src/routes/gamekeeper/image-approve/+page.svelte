bb1399c9e3288ec5058e239b5285f14c2ac63554
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2023-03-21 18:19:37 +0000

    my goodness.  MORE bug fixes?!
    it cannot be...

--- src/routes/gamekeeper/image-approve/+page.svelte
+++ src/routes/gamekeeper/image-approve/+page.svelte
@@ -36,29 +36,29 @@
 		<div class = "item" bind:this="{item[index]}">
 			{#if popup == true && currentItem == item[index]}
 				<div class="trash-size" on:mouseleave="{removeDiv}">
-					<form method="POST" action="?/accept">
+					<form method="POST" action="?/accept" bind:this={form}>
 						<input type="hidden" name="id" value={i.id}>
 						<input type="hidden" name="team" value={i.team}>
 						<input type="hidden" name="score" value=0>
 						<input type="hidden" name="carbon" value={70 + getRandomInt(10)}>
 						<input type="hidden" name="tm" value={i.monster_id}>
-						<button type="submit" class="trash">Small trash</button>
+						<button type="submit" class="trash" on:click={freezeForm}>Small trash</button>
 					</form>
-					<form method="POST" action="?/accept">
+					<form method="POST" action="?/accept" bind:this={form}>
 						<input type="hidden" name="id" value={i.id}>
 						<input type="hidden" name="team" value={i.team}>
 						<input type="hidden" name="score" value=1>
 						<input type="hidden" name="carbon" value={240 + getRandomInt(10)}>
 						<input type="hidden" name="tm" value={i.monster_id}>
-						<button type="submit" class="trash">Medium trash</button>
+						<button type="submit" class="trash" on:click={freezeForm}>Medium trash</button>
 					</form>
-					<form method="POST" action="?/accept">
+					<form method="POST" action="?/accept" bind:this={form}>
 						<input type="hidden" name="id" value={i.id}>
 						<input type="hidden" name="team" value={i.team}>
 						<input type="hidden" name="score" value=2>
 						<input type="hidden" name="carbon" value={490 + getRandomInt(10)}>
 						<input type="hidden" name="tm" value={i.monster_id}>
-						<button type="submit" class="trash">Large trash</button>
+						<button type="submit" class="trash" on:click={freezeForm}>Large trash</button>
 					</form>
 				</div>
 			{/if}
